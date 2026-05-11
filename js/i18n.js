bdd7930e4c9859a6f1f4b5c9ffe93f81adff7af0
E10DAN <danan.ethan@yahoo.ca>
2026-05-11 18:32:40 -0400

    Partners: rename to Our Partners, increase logo sizes to 64/72px

--- js/i18n.js
+++ js/i18n.js
@@ -76,7 +76,7 @@
     "svc4.bullet2": "Ideal for offices and bathrooms",
     "svc4.bullet3": "Custom finishes available",
 
-    "brands.label": "Our Brands",
+    "brands.label": "Our Partners",
 
     "area.eyebrow": "Service Area",
     "area.heading": "Serving Montreal &amp; Surrounding Areas",
@@ -265,7 +265,7 @@
     "svc4.bullet2": "Idéal pour bureaux et salles de bain",
     "svc4.bullet3": "Finitions sur mesure disponibles",
 
-    "brands.label": "Nos marques",
+    "brands.label": "Nos partenaires",
 
     "area.eyebrow": "Zone desservie",
     "area.heading": "Nous desservons Montréal &amp; les environs",
